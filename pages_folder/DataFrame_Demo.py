03bf686c182aa8e374b788a405579d04bba5c5ad
Vitor de Castro Silva <vitordecastrosilva@gmail.com>
2024-07-25 14:17:27 -0300

    init commit

---
+++ pages_folder/DataFrame_Demo.py
@@ -0,0 +1,39 @@
+# Copyright 2018-2022 Streamlit Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import streamlit as st
+import inspect
+import textwrap
+import pandas as pd
+import altair as alt
+from utils import show_code
+
+print("File dataframe_demo ", __name__)
+from urllib.error import URLError
+
+
+def data_frame_demo():
+    print("File plotting_demo ", __name__)
+
+
+st.markdown("# DataFrame Demo")
+st.sidebar.header("DataFrame Demo")
+st.write(
+    """This demo shows how to use `st.write` to visualize Pandas DataFrames.
+(Data courtesy of the [UN Data Explorer](http://data.un.org/Explorer.aspx).)"""
+)
+
+data_frame_demo()
+
+show_code(data_frame_demo)
---
+++ pages_folder/__init__.py
@@ -0,0 +1,4 @@
+from .DataFrame_Demo import data_frame_demo
+from .Mapping_Demo import test
+
+__all__ = ["data_frame_demo", "test"]
